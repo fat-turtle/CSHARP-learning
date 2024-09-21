70027e3c71b65748a04a44187c143e4f3fa77043
Luca <lucatheboss2016@gmail.com>
2024-09-21 19:59:32 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input A");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Input B");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
